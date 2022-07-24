af177991dad85bbd6dce9f2e443e191e1161805a
mb-tousif <meta.block.tousif@gmail.com>
2022-07-24 22:56:45 +0600

    Singup Coded

--- client/src/components/Login/LoginModal.js
+++ client/src/components/Login/LoginModal.js
@@ -1,6 +1,6 @@
-import { Box, Button, Dialog, styled, TextField, Typography } from '@mui/material';
+import { Box, Button, Dialog, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
-import login from '../../Assets/images/loginForm.png'
+import LoginImg from '../../Assets/images/loginForm.png'
 
 const LoginModal = ({ open, setOpen}) => {
   const accountInitialValues = {
@@ -15,10 +15,31 @@ const LoginModal = ({ open, setOpen}) => {
       subHeading: 'Sign Up with your Mobile number to get started'
     }
   }
-  
+  // const loginInitialValues = {
+  //   username: "",
+  //   password: "",
+  // };
+
+  const signupInitialValues = {
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    phone: "",
+  };
+  // const [login, setLogin] = useState(loginInitialValues);
+  const [signup, setSignup] = useState(signupInitialValues);
+  // const [error, showError] = useState(false);
   const [account, toggleAccount] = useState(accountInitialValues.login);
   const toggleSignup = () => {
     toggleAccount(accountInitialValues.signup);
+  };
+
+  const onInputChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+    console.log(signup);
   };
 
   const handleClose = () => {
@@ -32,7 +53,7 @@ const LoginModal = ({ open, setOpen}) => {
         <Box sx={{ background: "#321575", height: "100%", border: "5px", width: "40%", color: "rgb(249 250 251)", overflow: "hidden" }}>
           <Typography sx={{ padding: "10%"}} variant='h5'>{account.heading}</Typography>
           <Typography sx={{ padding: "10%"}}>{account.subHeading}</Typography>
-          <Box sx={{display:"flex", justifyItem: "center", justifyContent: "center"}}><img src={login} width="70%" height="200" alt="log" /></Box>
+          <Box sx={{display:"flex", justifyItem: "center", justifyContent: "center"}}><img src={LoginImg} width="70%" height="200" alt="log" /></Box>
         </Box>
         {
           account.view === "login" ?
@@ -50,12 +71,12 @@ const LoginModal = ({ open, setOpen}) => {
         </Box>
         :
         <Box sx={{width: "60%",display: 'flex', flexDirection: 'column', padding: "25px 35px", flex: 1}}>
-          <TextField marginTop='20px' label="Enter First Name" variant="standard" />
-          <TextField marginTop='20px' label="Enter Last Name" variant="standard"/>
-          <TextField marginTop='20px' label="Enter Username" variant="standard"/>
-          <TextField marginTop='20px' label="Enter Email" variant="standard"/>
-          <TextField marginTop='20px' label="Enter Password" variant="standard"/>
-          <TextField marginTop='20px' label="Enter Phone Number" variant="standard"/>
+          <TextField marginTop='20px' onBlur={(e) => onInputChange(e)} name="firstName" label="Enter First Name" variant="standard" />
+          <TextField marginTop='20px' onBlur={(e) => onInputChange(e)} name="lastName" label="Enter Last Name" variant="standard"/>
+          <TextField marginTop='20px' onBlur={(e) => onInputChange(e)} name="username" label="Enter Username" variant="standard"/>
+          <TextField marginTop='20px' onBlur={(e) => onInputChange(e)} name="email" label="Enter Email" variant="standard"/>
+          <TextField marginTop='20px' onBlur={(e) => onInputChange(e)} name="password" label="Enter Password" variant="standard"/>
+          <TextField marginTop='20px' onBlur={(e) => onInputChange(e)} name="phone" label="Enter Phone Number" variant="standard"/>
           <Button style={{color:"#fff", marginTop: "20px", background: "#8d0b93",textTransform: "none"}}>Continue</Button>
           {/* <Typography style={{cursor:"pointer", marginTop:'20px', textAlign: "center", color:"#321575"}} variant="caption">New to General Shop? Create an account</Typography> */}
         </Box>
